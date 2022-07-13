254d578c64729d88b9b7692b69b83cd14701b752
kiberd <34852597+kiberd@users.noreply.github.com>
2022-07-13 16:11:39 +0900

    vercel 환경변수 등록

--- api/posts.js
+++ api/posts.js
@@ -2,13 +2,6 @@ import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const getFakePost = async (limit) => {
-
-    const { data } = await axios(`https://jsonplaceholder.typicode.com/photos?&_limit=${10}`);
-
-    return data;
-};
-
 export const getPost = async (offset) => {
 
     const { data } = await axios(BASE_URL + `/view/${offset}/10`);
@@ -16,9 +9,16 @@ export const getPost = async (offset) => {
     return data;
 };
 
-export const getMorePost = async (start) => {
+// export const getFakePost = async (limit) => {
 
-    const { data } = await axios(`https://jsonplaceholder.typicode.com/todos?_start=${start}&_limit=10`);
+//     const { data } = await axios(`https://jsonplaceholder.typicode.com/photos?&_limit=${10}`);
+
+//     return data;
+// };
+
+// export const getMorePost = async (start) => {
+
+//     const { data } = await axios(`https://jsonplaceholder.typicode.com/todos?_start=${start}&_limit=10`);
     
-    return data;
-}
+//     return data;
+// }
