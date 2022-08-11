79a49b9f03111199234d493c31b6ce07308d76b2
kiberd <kiberd22@gmail.com>
2022-08-11 13:54:17 +0900

    api route 변경

--- api/user.js
+++ api/user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const addUser = async (user) => {
-	const { data } = await axios.post(`/api/user/create`, {
+	const { data } = await axios.post(`/apis/user/create`, {
 		user: user,
 	});
 
@@ -10,7 +10,7 @@ export const addUser = async (user) => {
 
 
 export const getUser = async (email) => {
-	const { data } = await axios.post(`/api/user/read`, {
+	const { data } = await axios.post(`/apis/user/read`, {
 		email: email,
 	});
 
