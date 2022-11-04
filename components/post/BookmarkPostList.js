f275b71c04e24e8db273645646a655cb3c0275fb
kiberd <kiberd22@gmail.com>
2022-11-04 15:32:56 +0900

    loading spinner 적용

--- components/post/BookmarkPostList.js
+++ components/post/BookmarkPostList.js
@@ -5,6 +5,7 @@ import { userState } from "../../atoms/auth";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { searchFilterState } from "../../atoms/search";
 import BookmarkPost from './BookmarkPost';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const BookmarkPostList = () => {
 
@@ -68,18 +69,35 @@ const BookmarkPostList = () => {
         }
     }, [filter]);
 
+    if (isFetching || isLoading) return (
+        <div class="bg-white dark:bg-black">
+            <div class="max-w-2xl mx-auto py-2 px-4 sm:py-5 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div class="flex justify-center items-center h-[70vh]">
+                    <ClipLoader
+                        // color={color}
+                        loading={true}
+                        // cssOverride={override}
+                        size={50}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <div class="bg-white dark:bg-black">
             <div class="max-w-2xl mx-auto py-2 px-4 sm:py-5 sm:px-6 lg:max-w-7xl lg:px-8">
                 <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 p-3">
                     {
                         filterData
-                            ? filterData.map((data, index) => (
-                                <BookmarkPost key={index} data={data} />
+                            ? filterData.map((filterdData, index) => (
+                                <BookmarkPost key={index} data={filterdData} />
                             ))
                             : posts ?
-                                posts.map((data, index) => (
-                                    <BookmarkPost key={index} data={data} />
+                                posts.map((filterdData, index) => (
+                                    <BookmarkPost key={index} data={filterdData} />
                                 )) : null
                     }
                 </div>
